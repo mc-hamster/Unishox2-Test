e20d663299eabf49478e751a7ddfc9ea35c1382e
Jm Casler <jm@casler.org>
2022-03-03 18:00:24 -0800

    Partial work

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,4 @@
-/** NimBLE_Secure_Server Demo:
- *
- * This example demonstrates the secure passkey protected conenction and communication between an esp32 server and an esp32 client.
- * Please note that esp32 stores auth info in nvs memory. After a successful connection it is possible that a passkey change will be ineffective.
- * To avoid this clear the memory of the esp32's between security testings. esptool.py is capable of this, example: esptool.py --port /dev/ttyUSB0 erase_flash.
- *
- *  Created: on Jan 08 2021
- *      Author: mblasee
- */
+
 
 #include <Arduino.h>
 #include "mesh/compression/unishox2.h"
@@ -18,12 +10,18 @@ void setup()
   Serial.println("Meshtastic Compression Test");
 
 
-  char *message[] = {"Hello friends!"};
-  uint32_t len = 20;
-  char *output[] = {};
-  int result = unishox2_compress_simple(*message, len, *output);
-  Serial.println(*message);
-  Serial.println(*output);
+  int ilen,olen;
+  char out[100]={0};
+  char *in=(char*)"GPVTG,182.66,T,,M,0.002,N,0.004,K,A*30";
+
+  ilen=strlen(in);
+  olen=unishox2_compress_simple(in,ilen,out);
+
+  Serial.println(ilen);
+  Serial.println(in);
+  Serial.println(olen);
+  Serial.println(out);
+
 }
 
 void loop()
