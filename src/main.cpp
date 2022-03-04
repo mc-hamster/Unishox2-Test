b8ae7d3981f381306d5a6804e9dc4f773c704df5
Jm Casler <jm@casler.org>
2022-03-03 18:46:04 -0800

    Updated test string + Formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,31 @@
 
 
 #include <Arduino.h>
-extern "C" {
-    #include "mesh/compression/unishox2.h"
+extern "C"
+{
+#include "mesh/compression/unishox2.h"
 }
-
-
 
 void setup()
 {
+
+  delay(100);
+
   Serial.begin(115200);
   Serial.println("Meshtastic Compression Test");
 
+  int ilen, olen;
+  char out[100] = {0};
+  //char *in = (char *)"GPVTG,182.66,T,,M,0.002,N,0.004,K,A*30";
+  char *in = (char *)"Hiiiiiiii! :) How are you doing? I am doing well.";
 
-  int ilen,olen;
-  char out[100]={0};
-  char *in=(char*)"GPVTG,182.66,T,,M,0.002,N,0.004,K,A*30";
-
-  ilen=strlen(in);
-  olen=unishox2_compress_simple(in,ilen,out);
+  ilen = strlen(in);
+  olen = unishox2_compress_simple(in, ilen, out);
 
   Serial.println(ilen);
   Serial.println(in);
   Serial.println(olen);
   Serial.println(out);
-
 }
 
 void loop()
