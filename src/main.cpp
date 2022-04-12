c0f48d5fab6b1f3979b1fb5ef41b70abfe46351f
Jm Casler <jm@casler.org>
2022-04-11 19:18:34 -0700

    Full compress & decompress

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,26 @@ void setup()
   Serial.begin(115200);
   Serial.println("Meshtastic Compression Test");
 
-  int ilen, olen;
-  char out[100] = {0};
-  char *in = (char *)"Hiiiiiiii! :) How are you doing? I am doing well.";
+  int orig_len, compressed_len;
+  char compressed_out[100] = {0};
+  char *orig = (char *)"Hiiiiiiii! :) How are you doing? I am doing well.";
+  //char *orig = (char *)"Meshtastic";
 
-  ilen = strlen(in);
-  olen = unishox2_compress_simple(in, ilen, out);
+  orig_len = strlen(orig);
+  compressed_len = unishox2_compress_simple(orig, orig_len, compressed_out);
 
-  Serial.println(ilen);
-  Serial.println(in);
-  Serial.println(olen);
-  Serial.println(out);
+  Serial.println(orig_len);
+  Serial.println(orig);
+  Serial.println(compressed_len);
+  Serial.println(compressed_out);
+
+  char decompressed_out[100] = {};
+  int decompressed_len;
+
+  decompressed_len = unishox2_decompress_simple(compressed_out, compressed_len, decompressed_out);
+
+  Serial.println(decompressed_len);
+  Serial.println(decompressed_out);
 }
 
 void loop()
